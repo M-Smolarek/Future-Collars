f78f2e55db9cfdc20ff00a852699144b6659f8b5
M-Smolarek <michal.smolarek@tlen.pl>
2024-06-26 18:13:08 +0200

    u

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-const form_1 = document.getElementById("active_field_1_id");
-const form_1_result = document.getElementById("input_1_result");
+const var_form = document.getElementById("active_field_id");
 
-const form_2 = document.getElementById("active_field_2_id");
-const form_2_result = document.getElementById("input_2_result");
+const form_result = document.getElementById("result_id");
 
-const pow = (form_1_result, form_2_result) => {
-  return form_1(value) ** form_2(value);
+const pow = (a, b) => {
+  return a ** b;
 };
 
-form_1.addEventListener("submit", (event) => {
+var_form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const value = event.target.input_1_name.value.trim();
-});
-
-form_2.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const value = event.target.input_2_name.value.trim();
+  const a_value = event.target.input_1_name.value.trim();
+  const b_value = event.target.input_2_name.value.trim();
+  form_result.textContent = pow(a_value, b_value);
 });
