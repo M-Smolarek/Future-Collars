47761bfe83844f5527f98ed977624b29e1238145
M-Smolarek <michal.smolarek@tlen.pl>
2024-06-26 16:30:44 +0200

    u

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const form = document.getElementById("form_id");
-const result = document.getElementById("input_result");
+const var_form = document.getElementById("form_id");
+const var_result = document.getElementById("input_result");
 
 const sayHello = (parameter_name) => {
   return `Hello ${parameter_name || "world"}`;
 };
 
-form.addEventListener("submit", (event) => {
+var_form.addEventListener("submit", (event) => {
   event.preventDefault();
   const value = event.target.input_name.value.trim();
-  result.textContent = sayHello(value);
+  var_result.textContent = sayHello(value);
+  var_result.style.color = "red";
 });
