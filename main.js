5c453b67333d401c77f6361f0c3c7cb503191671
M-Smolarek <michal.smolarek@tlen.pl>
2024-06-27 17:27:18 +0200

    u

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const elements = [1, 2, 3, 4, 5];
 
-let multiplier = 2;
+const multiplierValue = 2;
 
-const multiplyBy = elements.map((number) => {
-  for (let i = 0; i < elements.length; i++) {
-    return number * multiplier;
-  }
-});
-console.log(multiplyBy);
+const multiplyBy = (items, multiplier) => {
+  return items.map((item) => {
+    return item * multiplier;
+  });
+};
+console.log(multiplyBy(elements, multiplierValue));
