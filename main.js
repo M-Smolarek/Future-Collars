60f067e954f1040bbefe7aa547c9f6f28bb0d0d4
M-Smolarek <michal.smolarek@tlen.pl>
2024-06-28 09:01:28 +0200

    u

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const var_form = document.getElementById("active_field_id");
+const varForm = document.getElementById("activeFieldId");
 
-const form_result = document.getElementById("result_id");
+const formResult = document.getElementById("resultId");
 
-const pow = (a, b) => {
+const rectangleArea = (a, b) => {
   return a * b;
 };
 
-var_form.addEventListener("submit", (event) => {
+varForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const a_value = event.target.input_1_name.value.trim();
-  const b_value = event.target.input_2_name.value.trim();
-  form_result.textContent = pow(a_value, b_value);
+  const aValue = event.target.input1Name.value.trim();
+  const bValue = event.target.input2Name.value.trim();
+  formResult.textContent = rectangleArea(aValue, bValue);
 });
