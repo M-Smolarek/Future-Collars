2a5bcccb56972d5be2f8e268d288621b55fa75f0
M-Smolarek <michal.smolarek@tlen.pl>
2024-06-28 06:27:42 +0200

    u

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const elements = [1, 2, 3, 4, 5];
 
-const iterateAndSum = elements.reduce((acc, number) => {
-  return acc + number;
-}, 0);
-console.log(iterateAndSum);
+const iterateAndSum = (items) => {
+  return items.reduce((acc, number) => {
+    return acc + number;
+  }, 0);
+};
+console.log(iterateAndSum(elements));
