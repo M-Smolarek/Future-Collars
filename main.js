03c87faa2570d347339d6b5db189a0bffdf4dd7a
M-Smolarek <michal.smolarek@tlen.pl>
2024-06-28 07:11:32 +0200

    u

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const elements_1 = [1, 2, -3, 4, 5];
+const elements1 = [1, 2, -3, 4, 5];
 
-const elements_2 = [-6, 7, -9, 10];
+const elements2 = [-6, 7, -9, 10];
 
-const filterItems_1 = elements_1.filter((number) => {
-  return number > 0;
-});
+const combineElements = [...elements1, ...elements2];
 
-const filterItems_2 = elements_2.filter((number) => {
-  return number > 0;
-});
+const filterItems = (sumArray) => {
+  return sumArray.filter((number) => {
+    return number > 0;
+  });
+};
 
-const filterItems = [filterItems_1, filterItems_2];
-console.log(filterItems);
+console.log(filterItems(combineElements));
