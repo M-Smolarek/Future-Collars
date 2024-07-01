6d0d354c57d43ff0a7eb618e485c8db837c22bb1
M-Smolarek <michal.smolarek@tlen.pl>
2024-07-01 20:12:54 +0200

    u

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const elements1 = [1, 2, -3, 4, 5];
 const elements2 = [-6, 7, -9, 10];
 
 const filterItems = (a, b) => {
-  combineElements = [...elements1, ...elements2];
-  return sumArray.filter((combineElements) => {
-    return combineElements > 0;
+  const combineElements = [...elements1, ...elements2];
+  return combineElements.filter((item) => {
+    return item > 0;
   });
 };
 
