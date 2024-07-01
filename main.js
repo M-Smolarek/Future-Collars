db5c7679568246ffd4e624890124bd25182590ed
M-Smolarek <michal.smolarek@tlen.pl>
2024-07-01 20:15:11 +0200

    u

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const elements1 = [1, 2, -3, 4, 5];
 const elements2 = [-6, 7, -9, 10];
 
 const filterItems = (a, b) => {
-  const combineElements = [...elements1, ...elements2];
+  const combineElements = [...a, ...b];
   return combineElements.filter((item) => {
     return item > 0;
   });
