c96e6e9c58b0a82c40f81d12f73501581c0d8ba2
M-Smolarek <michal.smolarek@tlen.pl>
2024-07-01 16:55:57 +0200

    u

---
+++ main.js
@@ -0,0 +1,22 @@
+// document.addEventListener("DOMContentLoaded", function () {});
+//   const przykladowyTekst = document.getElementById("h1Id");
+//   console.log(przykladowyTekst);
+// });
+
+const przykladowyTekst = "Przykładowy tekst";
+
+const przykladowyTekst2 = document.getElementById("h1Id");
+
+console.log(przykladowyTekst);
+
+console.log(przykladowyTekst2);
+
+const fillElement = document.createElement (a) => {
+ return `Element <span>${parameter_name}</span>`;
+ console.log(fillElement(przykladowyTekst2));
+};
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   const przykladowyTekst = document.getElementById("h1Id");
+//   console.log(przykladowyTekst);
+// });
