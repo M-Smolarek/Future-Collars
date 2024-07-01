8bc2d7dd290efe788f9c3565ff7944f6491135d1
M-Smolarek <michal.smolarek@tlen.pl>
2024-07-01 17:17:37 +0200

    u

--- main.js
+++ main.js
@@ -7,14 +7,21 @@ const przykladowyTekst = "Przykładowy tekst";
 
 const przykladowyTekst2 = document.getElementById("h1Id");
 
-console.log(przykladowyTekst);
+// console.log(przykladowyTekst);
 
-console.log(przykladowyTekst2);
+// console.log(przykladowyTekst2);
 
-const fillElement = document.createElement (a) => {
- return `Element <span>${parameter_name}</span>`;
- console.log(fillElement(przykladowyTekst2));
-};
+// const fillElement = document.createElement (a) => {
+//  return `Element <span>${parameter_name}</span>`;
+//  console.log(fillElement(przykladowyTekst2));
+// };
+
+function fillElement(a) {
+  const spanStart = document.createElement("<span>");
+  const spanEnd = document.createElement("</span>");
+  return `Element, spanStart, a, spanEnd`;
+  console.log(fillElement(przykladowyTekst));
+}
 
 // document.addEventListener("DOMContentLoaded", function (event) {
 //   const przykladowyTekst = document.getElementById("h1Id");
