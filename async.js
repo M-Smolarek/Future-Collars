5a18349dd881df988528a1acb33963b95e5c7bee
M-Smolarek <michal.smolarek@tlen.pl>
2024-08-02 17:26:30 +0200

    u

---
+++ async.js
@@ -0,0 +1,1 @@
+// Kod js z atrybutem async bedzie ładowany równolegle do parsowania kodu html i zostanie zrealizowany natychmiast po pobraniu kodu js async
---
+++ defer.js
@@ -0,0 +1,1 @@
+// Kod js z atrybutem async bedzie ładowany równolegle do parsowania kodu html i zostanie zrealizowany dopiero po pobraniu przez przegladarke kodu html
