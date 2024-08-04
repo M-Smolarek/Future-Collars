62f6d107e36be6d86049d5c4c14123d471da7289
M-Smolarek <michal.smolarek@tlen.pl>
2024-08-04 18:26:07 +0200

    u

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const button = document.getElementById("getCurrencies");
+const showCurrencies = document.getElementById("getCurrencies");
+const showRate = document.getElementById("getRate");
+showRate.style.display = "none";
 
-button.addEventListener("click", () => {
+const select = document.createElement("select");
+
+const result = document.createElement("h2");
+result.classList.add("result");
+
+let rates = {};
+
+showCurrencies.addEventListener("click", () => {
   fetch("https://api.frankfurter.app/latest")
     .then((response) => {
       if (!response.ok) {
@@ -12,8 +21,28 @@ button.addEventListener("click", () => {
       if (!data.rates) {
         return Promise.reject();
       }
+      rates = data.rates; //w wierszach 20-35 mamy dostep do danych z API
+      select.replaceChildren();
+      const currencies = Object.keys(data.rates);
+      currencies.forEach((item) => {
+        const option = document.createElement("option");
+        option.value = item;
+        option.textContent = item;
+        select.appendChild(option);
+      });
+      document.body.appendChild(select);
+      showRate.style.display = "inline-block";
     })
     .catch((error) => {
       alert("Wystąpił błąd");
     });
+});
+
+showRate.addEventListener("click", () => {
+  if (!select.value) {
+    return;
+  }
+  const rate = rates[select.value]; //szukamy elementu rates dla wybranego klucza z listy rozwijanej(select)
+  result.textContent = `EUR/${select.value}: ${rate}`;
+  document.body.appendChild(result);
 });
