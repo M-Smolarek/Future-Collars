e13d970d01d5c545df9bdbc0d691c9b43b934625
M-Smolarek <michal.smolarek@tlen.pl>
2024-08-08 20:16:33 +0200

    u

---
+++ main.js
@@ -0,0 +1,11 @@
+class Apple {
+  constructor(name, color, sweetness) {
+    this.name = name;
+    this.color = color;
+    this.sweetness = sweetness;
+  }
+}
+
+const apple = new Apple("antonowka", "yellow", 5);
+
+console.log(apple);
