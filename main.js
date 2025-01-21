b52ce3d34b342141f9f7f8640593eb9b859ff093
M-Smolarek <michal.smolarek@tlen.pl>
2025-01-21 21:18:33 +0100

    u

---
+++ main.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("converter");
+const result = document.getElementById("result");
+const errorMessage = document.getElementById("errorMessage");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  errorMessage.textContent = "";
+  result.textContent = "";
+  const button = event.target.convert;
+  button.disabled = true;
+
+  const amount = event.target.amount.value;
+  const currency = event.target.currency.value;
+
+  const url = `https://api.nbp.pl/api/exchangerates/rates/A/${currency}/today/`;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject();
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (
+        data?.rates?.length > 0 &&
+        //        typeof data.rates[0].mid === "number" &&
+        data.rates[0].mid > 0
+      ) {
+        const rate = data.rates[0].mid;
+        result.textContent = (amount * rate).toFixed(2);
+      } else {
+        return Promise.reject();
+      }
+    })
+    .catch(() => {
+      errorMessage.textContent = "Błąd pobieranych danych";
+    })
+    .finally(() => {
+      button.disabled = false;
+    });
+});
